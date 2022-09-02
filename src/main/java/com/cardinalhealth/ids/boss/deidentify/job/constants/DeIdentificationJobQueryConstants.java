9a88b30aebc2614e2403c6855a1f7d6b82aa5236
cah-mohan-arora <mohan.arora@cardinalhealth.com>
2022-09-02 16:40:20 +0530

    Query modified - Set limit 10

--- src/main/java/com/cardinalhealth/ids/boss/deidentify/job/constants/DeIdentificationJobQueryConstants.java
+++ src/main/java/com/cardinalhealth/ids/boss/deidentify/job/constants/DeIdentificationJobQueryConstants.java
@@ -14,13 +14,13 @@ public class DeIdentificationJobQueryConstants {
 	@NoArgsConstructor
 	public static class JobQueryConstants {
 
-		public static final String READ_PATIENT = "SELECT pt.* FROM boss_fusion.ptnt pt";
+		public static final String READ_PATIENT = "SELECT pt.* FROM boss_fusion.ptnt pt limit 10";
 		
 		public static final String UPDATE_PATIENT = "UPDATE boss_fusion.ptnt\r\n" + 
 				" SET ptnt_id =:patientId \r\n" + 
 				" WHERE ptnt_num=:patientNum";
 		
-		public static final String READ_UTILIZATION_DRG = "SELECT ud.* FROM boss_fusion.utlz_drg ud";
+		public static final String READ_UTILIZATION_DRG = "SELECT ud.* FROM boss_fusion.utlz_drg ud limit 10";
 		
 		public static final String UPDATE_UTILIZATION_DRG = "UPDATE boss_fusion.utlz_drg \r\n" + 
 				" SET ptnt_id =:patientId \r\n" + 
